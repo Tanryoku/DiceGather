590a58e117767eafe77c7147e7bf28ba38713631
Jeremy <jmaubon@gmail.com>
2024-06-20 14:09:47 +0200

    mise à jour connexion BdD avec ID en ligne

--- Site/PHP/BdD_login.php
+++ Site/PHP/BdD_login.php
@@ -1,10 +1,7 @@
 <?php 
     const HOST = "localhost";
     const PORT = "3306";
-    const DBNAME = "dicegather";
-    const DBUSER = "root";
-    const DBPASS = "Il3vW4mp!";
-
+    const DBNAME = "jwjnpgwv_maubon_projet";
+    const DBUSER = "jwjnpgwv_maubon";
+    const DBPASS = "/Maubon_J84/";
     $dsn = "mysql:host=" . HOST . ";port=" . PORT . ";dbname=" . DBNAME;
-
-?>
--- Site/include/header.inc.php
+++ Site/include/header.inc.php
@@ -4,8 +4,8 @@
                 <div id="logo_contain"><img src="./assets/img/logo/logo_dice-gather.jpg" alt="logo Dice Gather"></div>
                 <h6 id="headerTitle">Dice Gather</h6>
                 <ul>
-                    <a href="connexion.php">Se connecter</a>
-                    <a href="rejoindreRoliste.php">Rejoindre les rôlistes</a>
+                    <li><a href="connexion.php">Se connecter</a></li>
+                    <li><a href="rejoindreRoliste.php">Rejoindre les rôlistes</a></li>
                 </ul>
             </section>
             <span id="nav_opener" aria-label="bouton d'ouverture menu navigation">&#8677;</span>
